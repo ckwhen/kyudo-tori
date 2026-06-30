80e04ac0a70a2de892b7e3ae6463464ef5f37edb
Chih Kai Wen <ckwen1988@gmail.com>
2026-06-30 11:44:05 +0800

    feat: add basic layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <main>
+    <div className="w-full flex flex-col">
+      <main className="max-w-6xl w-full mx-auto px-6 py-12 md:py-16">
         kyudo tori
       </main>
     </div>
