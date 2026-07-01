011f59a4e8a3d8edd22d5e5ffdcc65787848df2a
Chih Kai Wen <ckwen1988@gmail.com>
2026-07-01 18:02:58 +0800

    feat: update Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,37 @@
 import { shinsaService, ShinsaDashboard } from '@/features/shinsa';
+import { constants } from '@/shared/utils';
 
-export default async function Home() {
-  const shinsas = await shinsaService.getFilteredShinsas();
+const { SHINSA_PAGE_LIMIT } = constants;
+
+export const revalidate = 0;
+
+type Props = {
+  searchParams: Promise<{ page?: string }>,
+};
+
+export default async function Home({ searchParams }: Props) {
+  const params = await searchParams;
+  const currentPage = Math.max(1, parseInt(params.page || '1', 10));
+  const computedOffset = (currentPage - 1) * SHINSA_PAGE_LIMIT;
+
+  const [shinsas, totalCount] = await Promise.all([
+    shinsaService.getFilteredShinsas({
+      offset: computedOffset,
+      limit: SHINSA_PAGE_LIMIT,
+    }),
+    shinsaService.getShinsasCount(),
+  ]);
 
   return (
     <div className="w-full flex flex-col">
       <main className="max-w-6xl w-full mx-auto px-6 py-12 md:py-16">
         <ShinsaDashboard
           data={shinsas}
+          pagination={{
+            offset: computedOffset,
+            limit: SHINSA_PAGE_LIMIT,
+            count: totalCount,
+          }}
         />
       </main>
     </div>
